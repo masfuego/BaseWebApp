4a301cfc6ae4733af2c765a8bc76640265a40578
masfuego <morrisongrandon@gmail.com>
2022-11-22 15:41:55 +0000

    Change index.ejs to index.html

---
+++ project/js/signup.js
@@ -0,0 +1,13 @@
+// function openForm() {
+//     document.getElementById("myForm").style.display = "block";
+//   }
+  
+//   function closeForm() {
+//     document.getElementById("myForm").style.display = "none";
+//   }
+
+  const register = () => {
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    console.log(email, password)
+  }
